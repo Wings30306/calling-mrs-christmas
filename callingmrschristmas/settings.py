cbb3ec8d5315380a0480e3ff6e7042742abb4d69
Joke Heyndels <jo_hannah@outlook.com>
2019-11-23 12:13:08 +0100

    Add middleware to help with staticfiles on Heroku

--- callingmrschristmas/settings.py
+++ callingmrschristmas/settings.py
@@ -55,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #ADDITIONAL MIDDLEWARE
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'callingmrschristmas.urls'
