8f4d0960bef4e999c16ab40c40f3e37fc07b0adc
Joke Heyndels <jo_hannah@outlook.com>
2019-11-24 21:17:18 +0100

    Edit Employee to try and display profile pic

--- about/models.py
+++ about/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.files.storage import default_storage as storage
+from PIL import Image, ImageOps
 
 # Create your models here.
 from django.contrib.auth.models import User
@@ -9,3 +11,22 @@ class Employee(models.Model):
     intro = models.TextField()
     profile_pic = models.ImageField()
     is_staff = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        """
+        - installed 'django-cleanup' to auto-remove old image.
+        - installed 'pillow' to resize larger images.
+        - resizes all image formats except '.gif' as these cannot be resized
+        """
+        super(Employee, self).save(*args, **kwargs)
+        if self.profile_pic:
+            extension = "png"
+            img = Image.open(self.profile_pic)
+            new = ImageOps.fit(img, (500, 500))
+            temp = storage.open(self.profile_pic.name, "w")
+            new.save(temp.name, extension)
+            temp.close()
+            # continue if image format is not .gif
+            super(Employee, self).save(*args, **kwargs)
+
+    
