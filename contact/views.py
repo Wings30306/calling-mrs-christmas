80261790eaa6ac885ccad2b158a44b9d66fab0a3
Joke Heyndels <jo_hannah@outlook.com>
2019-11-29 21:33:44 +0100

    Add views and templates for contact and services

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html")
--- services/views.py
+++ services/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def services_list_view(request, *args, **kwargs):
+    return render(request, "services_list.html")
+
+def services_detail_view(request, *args, **kwargs):
+    return render(request, "services_detail.html")
