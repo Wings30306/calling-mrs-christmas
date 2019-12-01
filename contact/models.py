292a4811633d7a2eb258b96cbcc13d2f1636f452
Joke Heyndels <jo_hannah@outlook.com>
2019-12-01 15:42:07 +0100

    Add Location model in app Contact

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    street = models.CharField(max_length=100, default="High Street")
+    number = models.DecimalField(max_digits=5, decimal_places=0, default="120")
+    postcode = models.CharField(max_length=12, default="HP1 3AR")
+    town = models.CharField(max_length=100, default="Hemel Hempstead")
+    latitude = models.DecimalField(max_digits=10, decimal_places=6, default=51.753658)
+    longitude = models.DecimalField(max_digits=10, decimal_places=6, default=-0.474900)
+    email = models.EmailField(default="info@callingmrschristmas.com")
+    image = models.ImageField()
