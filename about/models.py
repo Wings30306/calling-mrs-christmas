0a2b866ec4b69665375f16ade4a0e21ca397ca52
Joke Heyndels <jo_hannah@outlook.com>
2019-12-07 21:53:02 +0100

    Add CaseStudy and CaseStudyImage models

--- about/models.py
+++ about/models.py
@@ -12,5 +12,24 @@ class Employee(models.Model):
     profile_pic = models.ImageField()
     is_staff = models.BooleanField(default=True)
 
+class CaseStudy(models.Model):
+    title = models.CharField(max_length=100)
+    client_first_name = models.CharField(max_length=100)
+    client_last_name_initial = models.CharField(max_length=1, blank=True, null=True)
+    client_company = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return 'Case Study: ' + self.title
+
+class CaseStudyImage(models.Model):
+    casestudy = models.ForeignKey(CaseStudy, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField()
+    alt = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.alt
+
+
 
     
