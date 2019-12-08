fe6a3bba4c427239b3098c34bd460afd273302ba
Joke Heyndels <jo_hannah@outlook.com>
2019-12-08 22:07:19 +0100

    Add password reset urls (WIP) in accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from django.urls import path
-from .views import logout, login, register, profile, edit_profile
+from django.urls import path, include
+from .views import logout, login, register, user_profile, edit_profile
 
 app_name = "accounts"
 urlpatterns = [
     path('logout', logout, name="logout"),
     path('login', login, name="login"),
     path('register', register, name="register"),
-    path('profile', profile, name="profile"),
+    path('profile', user_profile, name="profile"),
     path('profile/edit', edit_profile, name="edit_profile"),
+    path('password-reset/', include('accounts.urls_reset'))
 ]
---
+++ accounts/urls_reset.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.conf.urls import url
+from django.urls import path, reverse_lazy
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+
+
+urlpatterns = [
+    path('', PasswordResetView.as_view(), {'post_reset_redirect': reverse_lazy('password_reset_done')}, name="password_reset"),
+    path('done', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('?<uidb64>[0-9A-Za-z]+)-<token>).+', PasswordResetConfirmView.as_view(), {'post_reset_confirm': reverse_lazy('password_reset_complete')}, name='password_reset_confirm'),
+    path('complete', PasswordResetCompleteView.as_view(), name='password_reset_complete')
+]
