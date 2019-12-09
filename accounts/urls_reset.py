897c8fb37ffc9d3fd0005adbd66b80fcc6403344
Joke Heyndels <jo_hannah@outlook.com>
2019-12-09 19:20:40 +0100

    Allow password reset (second attempt)

--- accounts/urls_reset.py
+++ accounts/urls_reset.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, 
 
 
 urlpatterns = [
-    path('', PasswordResetView.as_view(), {'post_reset_redirect': reverse_lazy('password_reset_done')}, name="password_reset"),
+    path('', PasswordResetView.as_view(), {'post_reset_redirect': reverse_lazy('accounts:password_reset_done')}, name="password_reset"),
     path('done', PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('?<uidb64>[0-9A-Za-z]+)-<token>).+', PasswordResetConfirmView.as_view(), {'post_reset_confirm': reverse_lazy('accounts:password_reset_complete')}, name='password_reset_confirm'),
     path('complete', PasswordResetCompleteView.as_view(), name='password_reset_complete')
