3729e3bace8bf5ecd7ce33f6239a0d5f7d87a9ae
Joke Heyndels <jo_hannah@outlook.com>
2019-12-10 20:36:21 +0100

    Get about/detail_view with slug instead of pk

--- about/urls.py
+++ about/urls.py
@@ -5,5 +5,5 @@ from .views import about_view, detail_view
 app_name = "about"
 urlpatterns = [
     path('', about_view, name="about_list"),
-    path('<int:id>', detail_view, name="about_detail"),
+    path('<slug:user>', detail_view, name="about_detail"),
 ]
