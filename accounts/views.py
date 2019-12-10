55f5d95b07426b437060b6f66b59f6572b8fe27c
Joke Heyndels <jo_hannah@outlook.com>
2019-12-10 21:32:12 +0100

    Add password-reset link to login page (accounts)

--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 
+
 @login_required
 def logout(request):
     """Log the user out"""
@@ -13,11 +14,13 @@ def logout(request):
     messages.success(request, "Thanks for visiting, see you again soon!")
     return redirect(reverse('index'))
 
+
 def register(request):
     """Allow new user to create account"""
     if request.user.is_authenticated:
         """"Prevent logged in user from going back to login page"""
-        messages.success(request, request.user.first_name + ", you are already signed in!")
+        messages.success(request, request.user.first_name +
+                         ", you are already signed in!")
         return redirect(reverse('index'))
     if request.method == "POST":
         """Register the user using form data"""
@@ -29,12 +32,14 @@ def register(request):
                                      password=request.POST['password1'])
             if user:
                 auth.login(user=user, request=request)
-                messages.success(request, "Welcome, " + user.first_name + "! Your account was successfully created.")
+                messages.success(request, "Welcome, " + user.first_name +
+                                 "! Your account was successfully created.")
                 return redirect(reverse('accounts:profile'))
             else:
-                registration_form.add_error(request("Sorry, we are unable to register your account at this time."))
+                registration_form.add_error(
+                    request("Sorry, we are unable to register your account at this time."))
 
-    else: 
+    else:
         registration_form = UserRegistrationForm
     context = {
         "form": registration_form,
@@ -45,11 +50,13 @@ def register(request):
     }
     return render(request, "form.html", context)
 
+
 def login(request):
     """Return Login page"""
     if request.user.is_authenticated:
         """"Prevent logged in user from going back to login page"""
-        messages.success(request, request.user.first_name + ", you are already signed in!")
+        messages.success(request, request.user.first_name +
+                         ", you are already signed in!")
         return redirect(reverse('accounts:profile'))
     if request.method == "POST":
         """Log in the user using form data"""
@@ -65,14 +72,17 @@ def login(request):
                 return redirect(reverse('accounts:profile'))
             else:
                 form.add_error(None, "Your username or password is incorrect.")
-    else: 
+    else:
         form = UserLoginForm
     context = {
         "form": form,
         "form_title": "Log In",
         "button_text": "Log me in",
         "wrong_page_redirect": "<p><strong>If you were redirected here after password reset, " +
-                               "please sign in again with your new password.</strong></p>" + "<p>You don't have an account yet? " +
+                               "please sign in again with your new password.</strong></p>" +
+                               "<p>If you forgot your password, you can " +
+                               "<a href='accounts/password-reset'>reset</a> it.</p>" +
+                               "<p>You don't have an account yet? " +
                                "You can <a href='/accounts/register'>sign up</a> instead.</p>"
     }
     return render(request, "form.html", context)
@@ -86,6 +96,7 @@ def user_profile(request):
         "profile": user
     }
     return render(request, 'profile.html', context)
+
 
 @login_required
 def edit_profile(request, username):
