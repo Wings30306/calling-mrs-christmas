da4f02723e052e66e6a456bac609804c628d0629
Joke Heyndels <jo_hannah@outlook.com>
2019-12-13 22:42:59 +0100

    Add ServiceCategory model in services

--- services/admin.py
+++ services/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Service
+from .models import Service, ServiceCategory
 
 admin.site.register(Service)
+admin.site.register(ServiceCategory)
--- services/models.py
+++ services/models.py
@@ -3,11 +3,26 @@ from django.core.files.storage import default_storage as storage
 
 # Create your models here.
 
+
+class ServiceCategory(models.Model):
+    title = models.CharField(max_length=100)
+    tagline = models.CharField(max_length=100)
+    description = models.TextField()
+    name_for_image = models.CharField(max_length=100)
+    img_alt = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
 class Service(models.Model):
     title = models.CharField(max_length=100)
     brief_description = models.TextField()
     detailed_description = models.TextField()
-    image = models.ImageField()
-    image_description = models.CharField(max_length=100)
+    img_name = models.CharField(max_length=100)
+    img_alt = models.CharField(max_length=100)
     price = models.DecimalField(decimal_places=2, max_digits=9, default=4.99)
     available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
