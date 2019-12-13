2bde0539a4b364f392b66a752caadcab26809f45
Joke Heyndels <jo_hannah@outlook.com>
2019-12-13 22:43:52 +0100

    Adjust urls / views to show ServiceCategory model

--- services/urls.py
+++ services/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import ServiceListView, ServiceDetailView
+from .views import ServiceCategoryListView, ServiceDetailView
 
 app_name = "services"
 urlpatterns = [
-    path('', ServiceListView.as_view(), name="services_list"),
+    path('', ServiceCategoryListView.as_view(), name="services_list"),
     path('<int:pk>', ServiceDetailView.as_view(), name="service_detail"),
 ]
--- services/views.py
+++ services/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
-from .models import Service
+from .models import Service, ServiceCategory
 
 # Create your views here.
-class ServiceListView(ListView):
-    queryset = Service.objects.all()
+class ServiceCategoryListView(ListView):
+    template_name = 'services/service_categories_list.html'
+    queryset = ServiceCategory.objects.all()
 
 class ServiceDetailView(DetailView):
     model = Service
