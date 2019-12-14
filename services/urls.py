26ea64cf83b69e1dce4d72e97d2ffcbadf981473
Joke Heyndels <jo_hannah@outlook.com>
2019-12-14 14:04:04 +0100

    Display service list by category

--- services/urls.py
+++ services/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
 from django.urls import path
-from .views import ServiceCategoryListView, ServiceDetailView
+from .views import ServiceCategoryListView, service_list_by_category_view, ServiceDetailView
 
 app_name = "services"
 urlpatterns = [
     path('', ServiceCategoryListView.as_view(), name="services_list"),
+    path('<slug:category>', service_list_by_category_view, name="services_list_by_cat"),
     path('<int:pk>', ServiceDetailView.as_view(), name="service_detail"),
 ]
--- services/views.py
+++ services/views.py
@@ -3,9 +3,24 @@ from django.views.generic import CreateView, DetailView, ListView, UpdateView, D
 from .models import Service, ServiceCategory
 
 # Create your views here.
+
+
 class ServiceCategoryListView(ListView):
+    """Show all the service categories and their description"""
     template_name = 'services/service-categories-list.html'
     queryset = ServiceCategory.objects.all()
+
+
+def service_list_by_category_view(request, category):
+    template_name = 'services/service-list-by-category.html'
+    obj = ServiceCategory.objects.get(name=category)
+    queryset = Service.objects.filter(category=obj.pk)
+    context = {
+        "obj": obj,
+        "queryset": queryset
+    }
+    return render(request, template_name=template_name, context=context)
+
 
 class ServiceDetailView(DetailView):
     model = Service
