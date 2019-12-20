2c0c51427db5fdbf421466fe3c89a1f061731f9d
Joke Heyndels <jo_hannah@outlook.com>
2019-12-20 23:06:52 +0100

    Format cart views

--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
+
+
 def view_cart(request):
     """ A view that renders the cart contents page """
-    return render(request, "cart.html")
+    template_name = "cart/cart.html"
+    return render(request, template_name)
+
 
 def add_to_cart(request, primary_key):
     """ Add a quantity of the specified product to the cart """
@@ -12,6 +16,7 @@ def add_to_cart(request, primary_key):
     cart[primary_key] = cart.get(primary_key, quantity)
     request.session['cart'] = cart
     return redirect(reverse('services:services_list'))
+
 
 def adjust_cart(request, primary_key):
     """Adjust the quantity of the specified product to the specified amount"""
