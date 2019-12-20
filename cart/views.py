f0d7158291072326f345b172a2ba8b9995002c88
Joke Heyndels <jo_hannah@outlook.com>
2019-12-20 19:23:43 +0100

    Create cart views (view, add_to and adjust)

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
+def view_cart(request):
+    """ A view that renders the cart contents page """
+    return render(request, "cart.html")
+
+def add_to_cart(request, primary_key):
+    """ Add a quantity of the specified product to the cart """
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+    cart[primary_key] = cart.get(primary_key, quantity)
+    request.session['cart'] = cart
+    return redirect(reverse('services:services_list'))
+
+def adjust_cart(request, primary_key):
+    """Adjust the quantity of the specified product to the specified amount"""
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[primary_key] = quantity
+    else:
+        cart.pop(primary_key)
+
+    request.session['cart'] = cart
+    return redirect('view_cart')
