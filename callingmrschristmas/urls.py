e7bf971a22ec1e3e94b7dc9e7c071fee82b97291
Joke Heyndels <jo_hannah@outlook.com>
2019-12-20 19:21:38 +0100

    Include cart urls in main urls

--- callingmrschristmas/urls.py
+++ callingmrschristmas/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from . import settings
 from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from about.views import index_view
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,7 +27,8 @@ urlpatterns = [
     path('services/', include('services.urls')),
     path('contact/', include('contact.urls')),
     path('accounts/', include('accounts.urls')),
+    path('cart/', include('cart.urls'))
 ]
- 
+
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
