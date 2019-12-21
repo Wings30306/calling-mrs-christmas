ca03d8a05060bf5f93a86424dbefa268752ee712
Joke Heyndels <jo_hannah@outlook.com>
2019-12-21 14:43:22 +0100

    Return cart in cart/contexts.py

--- cart/contexts.py
+++ cart/contexts.py
@@ -6,13 +6,15 @@ def cart_contents(request):
     """
     Ensures that the cart contents are available when rendering every page
     """
-    cart = request.session.get('cart', {})
+    cart = {}
     cart_items = []
     total = 0
     count = 0
-    for primary_key, quantity in cart_items:
+    for primary_key, quantity in cart.items():
         service = get_object_or_404(Service, pk=primary_key)
         total += quantity * service.price
         count += quantity
-        cart_items.append({"id": id, "quantity": quantity})
-    return {"cart_items": cart_items, "total": total, "product_count": count}
+        cart_items.append({"service": service, "quantity": quantity})
+    cart = {"cart_items": cart_items, "total": total, "count": count}
+    print(cart)
+    return cart
