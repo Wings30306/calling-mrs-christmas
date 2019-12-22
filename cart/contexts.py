bbb4c9437e3e00ac9b8a27508508500127116a07
Joke Heyndels <jo_hannah@outlook.com>
2019-12-22 14:16:03 +0100

    Adjust cart_contents to convert Decimal to number

--- cart/contexts.py
+++ cart/contexts.py
@@ -6,15 +6,20 @@ def cart_contents(request):
     """
     Ensures that the cart contents are available when rendering every page
     """
-    cart = {}
     cart_items = []
     total = 0
     count = 0
-    for primary_key, quantity in cart.items():
-        service = get_object_or_404(Service, pk=primary_key)
-        total += quantity * service.price
-        count += quantity
-        cart_items.append({"service": service, "quantity": quantity})
+    cart = request.session.get(
+        "cart", {"cart_items": cart_items, "total": total, "count": count})
+    if len(cart['cart_items']) > 0:
+        for item in cart['cart_items']:
+            primary_key = item['primary_key']
+            quantity = int(item['quantity'])
+            service = get_object_or_404(Service, pk=primary_key)
+            price = int(service.price)
+            total += quantity * price
+            count += quantity
+            cart_items.append({"primary_key": primary_key, "quantity": quantity})
     cart = {"cart_items": cart_items, "total": total, "count": count}
-    print(cart)
+    request.session["cart"] = cart
     return cart
