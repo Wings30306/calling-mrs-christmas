fa76ad821ef3dc9405c639c0be8f28498ef17529
Joke Heyndels <jo_hannah@outlook.com>
2019-12-31 21:29:56 +0100

    Create and register cart model "Cart"

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
+
+
+class Cart(models.Model):
+    """Save cart between sessions if user is logged in"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    cart = JSONField(null=True, blank=True, default=dict)
