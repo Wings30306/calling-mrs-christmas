55fc31e04e4857b664c97e79687f613faa7ab0f5
Joke Heyndels <jo_hannah@outlook.com>
2020-01-04 12:15:53 +0100

    Check if authenticated user has cart or set cart.

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,11 +1,25 @@
 from django.shortcuts import get_object_or_404
 from services.models import Service
+from .models import Cart
 
 
 def cart_contents(request):
     """
     Ensures that the cart contents are available when rendering every page
     """
+    if request.user.is_authenticated:
+        try:
+            cart_instance = Cart.objects.get(user=request.user)
+            print(cart_instance)
+            cart = cart_instance.cart
+            request.session["cart"] = cart
+        except Cart.DoesNotExist:
+            if request.session["cart"]:
+                cart = request.session["cart"]
+            else:
+                cart = {"cart_items": [], "total": 0, "count": 0}
+        finally:
+            request.session["cart"] = cart
     cart_items = []
     total = 0
     count = 0
@@ -13,29 +27,37 @@ def cart_contents(request):
         "cart", {"cart_items": cart_items, "total": total, "count": count})
     print(cart)
     if len(cart['cart_items']) > 0:
+        index = 0
         for item in cart['cart_items']:
             primary_key = item['primary_key']
-            quantity = int(item['quantity'])
-            service = get_object_or_404(Service, pk=primary_key)
-            price = service.price_in_p
-            title = service.title
-            img = service.img_name
-            alt = service.img_alt
-            url = service.get_absolute_url()
-            description = service.brief_description
-            item_total = quantity * price
-            total += item_total
-            count += quantity
-            cart_items.append({"primary_key": primary_key,
-                               "title": title,
-                               "price": price,
-                               "img": img,
-                               "alt": alt,
-                               "url": url,
-                               "quantity": quantity,
-                               "description": description,
-                               "item_total": item_total
-                               })
+            if item['quantity'] == '':
+                quantity = 0
+            else:
+                quantity = int(item['quantity'])
+            if quantity == 0:
+                cart['cart_items'].pop(index)
+            else:
+                service = get_object_or_404(Service, pk=primary_key)
+                price = service.price_in_p
+                title = service.title
+                img = service.img_name
+                alt = service.img_alt
+                url = service.get_absolute_url()
+                description = service.brief_description
+                item_total = quantity * price
+                total += item_total
+                count += quantity
+                cart_items.append({"primary_key": primary_key,
+                                   "title": title,
+                                   "price": price,
+                                   "img": img,
+                                   "alt": alt,
+                                   "url": url,
+                                   "quantity": quantity,
+                                   "description": description,
+                                   "item_total": item_total
+                                   })
+            index += 1
     cart = {"cart_items": cart_items, "total": total, "count": count}
     request.session["cart"] = cart
     return cart
--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render, redirect, reverse
 from .contexts import cart_contents
+from .models import Cart
 
 # Create your views here.
-
+def helper_check_user_cart(request):
+    """function to check whether authenticated user has cart"""
+    if request.user.is_authenticated:
+        try:
+            cart_instance = Cart.objects.get(user=request.user)
+            print(cart_instance)
+            cart = cart_instance.cart
+            request.session["cart"] = cart
+        except Cart.DoesNotExist:
+            cart = {"cart_items": [], "total": 0, "count": 0}
+        finally:
+            request.session["cart"] = cart
+    else:
+        cart = request.session["cart"]
+    return cart
 
 def view_cart(request):
     """ A view that renders the cart contents page """
@@ -12,27 +27,34 @@ def view_cart(request):
 
 def add_to_cart(request, primary_key):
     """Add a quantity of the specified product to the cart"""
-    cart = request.session.get("cart")
+    cart = helper_check_user_cart(request)
     quantity = request.POST.get("quantity")
     new_cart_item = {"primary_key": primary_key, "quantity": quantity}
     print(new_cart_item)
     index = 0
     for item in cart["cart_items"]:
         if item["primary_key"] == new_cart_item["primary_key"]:
-            quantity = int(item["quantity"])
+            if item["quantity"] == "":
+                quantity = 0
+            else:
+                quantity = int(item["quantity"])
             quantity += int(new_cart_item["quantity"])
             request.session["cart"]["cart_items"][index]["quantity"] = quantity
             cart_contents(request)
             return redirect(reverse('cart:view_cart'))
         index += 1
     cart['cart_items'].append(new_cart_item)
+    if request.user.is_authenticated:
+        Cart.objects.filter(user=request.user).update_or_create(
+            user=request.user,
+            cart=request.session["cart"])
     cart_contents(request)
     return redirect(reverse('cart:view_cart'))
 
 
 def adjust_cart(request, primary_key):
     """Adjust the quantity of the specified product to the specified amount"""
-    cart = request.session.get("cart")
+    cart = helper_check_user_cart(request)
     quantity = request.POST.get("quantity")
     new_cart_item = {"primary_key": primary_key, "quantity": quantity}
     print(new_cart_item)
@@ -46,4 +68,6 @@ def adjust_cart(request, primary_key):
                 request.session["cart"]["cart_items"][index]["quantity"] = quantity
         index += 1
     cart_contents(request)
+    if request.user.is_authenticated:
+        Cart.objects.filter(user=request.user).update(cart=request.session["cart"])
     return redirect(reverse('cart:view_cart'))
