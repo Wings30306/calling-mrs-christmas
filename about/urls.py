c517b515cf81d354c38c15b52272be8484d26179
Joke Heyndels <jo_hannah@outlook.com>
2020-01-07 19:42:41 +0100

    Add casestudies url to about urls

--- about/urls.py
+++ about/urls.py
@@ -1,9 +1,10 @@
-from django.contrib import admin
 from django.urls import path
-from .views import about_view, detail_view
+from .views import about_view, detail_view, casestudy_list_view
 
 app_name = "about"
 urlpatterns = [
+    path('casestudies', casestudy_list_view, name="casestudies"),
     path('', about_view, name="about_list"),
     path('<slug:user>', detail_view, name="about_detail"),
+
 ]
