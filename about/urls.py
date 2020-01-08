db7c94ccb751d4739063b6b534ab2e505972f2e9
Joke Heyndels <jo_hannah@outlook.com>
2020-01-08 09:33:10 +0100

    Re-order about urls

--- about/urls.py
+++ about/urls.py
@@ -3,8 +3,8 @@ from .views import about_view, detail_view, casestudy_list_view
 
 app_name = "about"
 urlpatterns = [
-    path('casestudies', casestudy_list_view, name="casestudies"),
     path('', about_view, name="about_list"),
+    path('casestudies', casestudy_list_view, name="casestudies"),
     path('<slug:user>', detail_view, name="about_detail"),
 
 ]
