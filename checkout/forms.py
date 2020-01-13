2668820f92ae331f8b64161a898084f7e3d1d6c3
Joke Heyndels <jo_hannah@outlook.com>
2020-01-13 09:47:33 +0100

    In checkout/forms.py, make form fields required

--- checkout/forms.py
+++ checkout/forms.py
@@ -7,13 +7,14 @@ class MakePaymentForm(forms.Form):
     MONTH_CHOICES = [(i, i) for i in range(1, 12+1)]
     YEAR_CHOICES = [(i, i) for i in range(2020, 2038)]
 
-    credit_card_number = forms.CharField(
-        label="Credit card number", required=False)
-    cvv = forms.CharField(label="Security code (CVV)", required=False)
+    credit_card_number = forms.CharField(max_length=16, min_length=16,
+                                         label="Credit card number")
+    cvv = forms.CharField(max_length=3, min_length=3,
+                          label="Security code (CVV)")
     expiry_month = forms.ChoiceField(
-        label='Month', choices=MONTH_CHOICES, required=False)
+        label='Month', choices=MONTH_CHOICES)
     expiry_year = forms.ChoiceField(
-        label='Year', choices=YEAR_CHOICES, required=False)
+        label='Year', choices=YEAR_CHOICES)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
 
 
