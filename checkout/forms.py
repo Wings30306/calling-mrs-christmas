75ee07c940ffa5727ca9c25a052ee6f430f0426f
Joke Heyndels <jo_hannah@outlook.com>
2020-01-13 22:30:58 +0100

    Undo changes to checkout form (MakePaymentForm)

--- checkout/forms.py
+++ checkout/forms.py
@@ -7,14 +7,14 @@ class MakePaymentForm(forms.Form):
     MONTH_CHOICES = [(i, i) for i in range(1, 12+1)]
     YEAR_CHOICES = [(i, i) for i in range(2020, 2038)]
 
-    credit_card_number = forms.CharField(max_length=16, min_length=16,
-                                         label="Credit card number")
-    cvv = forms.CharField(max_length=3, min_length=3,
-                          label="Security code (CVV)")
+    credit_card_number = forms.CharField(
+        label="Credit card number", required=False)
+    cvv = forms.CharField(label="Security code (CVV)",
+                          required=False)
     expiry_month = forms.ChoiceField(
-        label='Month', choices=MONTH_CHOICES)
+        label='Month', choices=MONTH_CHOICES, required=False)
     expiry_year = forms.ChoiceField(
-        label='Year', choices=YEAR_CHOICES)
+        label='Year', choices=YEAR_CHOICES, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
 
 
@@ -22,4 +22,4 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('full_name', 'phone_number', 'street_address1', 'street_address2',
-                  'town_or_city', 'county', 'country', 'postcode', 'country')
+                  'town_or_city', 'postcode', 'county', 'country')
