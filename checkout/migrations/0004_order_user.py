62cb9599656ffe91b32c989534af2161f9e5a00d
Joke Heyndels <jo_hannah@outlook.com>
2020-01-21 08:23:58 +0100

    format migrations file after pylint warning long line

--- checkout/migrations/0004_order_user.py
+++ checkout/migrations/0004_order_user.py
@@ -16,7 +16,10 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='order',
             name='user',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(
+                default=1,
+                on_delete=django.db.models.deletion.CASCADE,
+                to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
     ]
