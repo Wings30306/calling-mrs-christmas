47e5fbcd3648734baf89785634294e4d39338360
Joke Heyndels <jo_hannah@outlook.com>
2020-01-24 22:46:06 +0100

    Uncomment contact/urls.py

--- contact/urls.py
+++ contact/urls.py
@@ -1,7 +1,7 @@
-# from django.urls import path
-# from .views import contact_view
+from django.urls import path
+from .views import contact_view
 
-# app_name = "contact"
-# urlpatterns = [
-#     path('', contact_view, name="contact"),
-# ]
+app_name = "contact"
+urlpatterns = [
+    path('', contact_view, name="contact"),
+]
