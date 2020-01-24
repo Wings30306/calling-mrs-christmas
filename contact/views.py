d0f0c1d58b03f30920ad277eb4f020bec3cc3c32
Joke Heyndels <jo_hannah@outlook.com>
2020-01-24 22:47:25 +0100

    Remove classbased views in contact.views.py

--- contact/views.py
+++ contact/views.py
@@ -1,22 +1,6 @@
-from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
-from .models import Location
-from .forms import LocationModelForm
+from django.shortcuts import render, redirect
+from .models import Location, ContactMessage
+from .forms import ContactForm
 
 # Create your views here.
-class LocationListView(ListView):
-    queryset = Location.objects.all()
 
-class LocationDetailView(DetailView):
-    model = Location
-
-class LocationCreateView(CreateView):
-    template_name = 'contact/location_create.html'
-    form_class = LocationModelForm
-
-class LocationUpdateView(UpdateView):
-    template_name = 'contact/location_create.html'
-    form_class = LocationModelForm
-    model = Location
-
-class LocationDeleteView(DeleteView):
-    model = Location
