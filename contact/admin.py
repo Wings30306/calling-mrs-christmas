1f9e2286e97aa47af8cb11ce049d8be746813146
Joke Heyndels <jo_hannah@outlook.com>
2020-01-24 22:45:24 +0100

    register ContactMessage model in contact/admin.py

--- contact/admin.py
+++ contact/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Location
+from .models import Location, ContactMessage
 
 admin.site.register(Location)
+admin.site.register(ContactMessage)
