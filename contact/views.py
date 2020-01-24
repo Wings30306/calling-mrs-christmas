ae4321f31c1352f95c36fcc709b7abca6dabfb22
Joke Heyndels <jo_hannah@outlook.com>
2020-01-24 22:47:51 +0100

    add contact_view in contact/views.py

--- contact/views.py
+++ contact/views.py
@@ -3,4 +3,9 @@ from .models import Location, ContactMessage
 from .forms import ContactForm
 
 # Create your views here.
-
+def contact_view(request):
+    context = {
+        "form": ContactForm(),
+        "locations": Location.objects.all()
+    }
+    return render(request, "contact.html", context=context)
