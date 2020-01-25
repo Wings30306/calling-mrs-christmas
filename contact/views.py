b6bfd4061da4c4487d126661e5423bf69e8844f5
Joke Heyndels <jo_hannah@outlook.com>
2020-01-25 15:29:40 +0100

    add post function to save contact message

--- contact/views.py
+++ contact/views.py
@@ -1,24 +1,30 @@
-from django.shortcuts import render, redirect
-from .models import Location, ContactMessage
+from django.shortcuts import render, redirect, reverse
+from .models import Location
 from .forms import ContactForm
 
 # Create your views here.
 
 
 def contact_view(request):
-    if request.user.is_authenticated:
-        initial_data = {
-            "user": request.user,
-            "name": request.user.first_name + " " + request.user.last_name,
-            "email": request.user.email
-        }
-        form = ContactForm(
-            request.POST or None, initial=initial_data)
+    if request.method == "POST":
+        contact_form = ContactForm(request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+        return redirect(reverse('accounts:profile'))
     else:
-        form = ContactForm()
+        if request.user.is_authenticated:
+            initial_data = {
+                "user": request.user,
+                "name": request.user.first_name + " " + request.user.last_name,
+                "email": request.user.email
+            }
+            form = ContactForm(
+                request.POST or None, initial=initial_data)
+        else:
+            form = ContactForm()
 
-    context = {
-        "form": form,
-        "locations": Location.objects.all()
-    }
-    return render(request, "contact.html", context=context)
+        context = {
+            "form": form,
+            "locations": Location.objects.all()
+        }
+        return render(request, "contact.html", context=context)
