30ef3e6a283855ea8130229d563e9adca571a165
Joke Heyndels <jo_hannah@outlook.com>
2020-01-25 14:48:20 +0100

    Add initial data for ContactForm if user logged in

--- contact/views.py
+++ contact/views.py
@@ -3,9 +3,22 @@ from .models import Location, ContactMessage
 from .forms import ContactForm
 
 # Create your views here.
+
+
 def contact_view(request):
+    if request.user.is_authenticated:
+        initial_data = {
+            "user": request.user,
+            "name": request.user.first_name + " " + request.user.last_name,
+            "email": request.user.email
+        }
+        form = ContactForm(
+            request.POST or None, initial=initial_data)
+    else:
+        form = ContactForm()
+
     context = {
-        "form": ContactForm(),
+        "form": form,
         "locations": Location.objects.all()
     }
     return render(request, "contact.html", context=context)
