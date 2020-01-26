ad387ace8ae88036342a698d1a5bc306ba8ea385
Joke Heyndels <jo_hannah@outlook.com>
2020-01-26 14:52:51 +0100

    Add bootstrap classes to fields in ContactForm

--- contact/forms.py
+++ contact/forms.py
@@ -11,13 +11,19 @@ class ContactForm(forms.ModelForm):
         ]
         widgets = {"user": forms.HiddenInput(),
                    "name": forms.TextInput(attrs={
-                       "class": "form-control"
+                       "class": "form-control",
+                       "aria-label": "Your name",
+                       "placeholder": "Your name"
                    }),
                    "email": forms.EmailInput(attrs={
-                       "class": "form-control"
+                       "class": "form-control",
+                       "aria-label": "Your email",
+                       "placeholder": "Your email"
                    }),
                    "message": forms.Textarea(attrs={
                        "class": "form-control",
                        "rows": 10,
+                       "aria-label": "Your message",
+                       "placeholder": "Your message"
                    })
                    }
