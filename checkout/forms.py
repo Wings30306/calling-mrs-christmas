3e4f98a952b6e3cbff0b7456e14dbc79660c0341
Joke Heyndels <jo_hannah@outlook.com>
2020-01-26 16:27:03 +0100

    add widgets to form fields in checkout/forms.py

--- checkout/forms.py
+++ checkout/forms.py
@@ -8,9 +8,21 @@ class MakePaymentForm(forms.Form):
     YEAR_CHOICES = [(i, i) for i in range(2020, 2038)]
 
     credit_card_number = forms.CharField(
-        label="Credit card number", required=False)
+        label="Credit card number",
+        required=False,
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "aria-label": "Credit card number",
+            "placeholder": "Credit card number"
+        }))
     cvv = forms.CharField(label="Security code (CVV)",
-                          required=False)
+                          required=False,
+                          widget=forms.TextInput(attrs={
+                              "class": "form-control",
+                              "aria-label": "CVV",
+                              "placeholder": "CVV",
+                              "max-length": 5
+                          }))
     expiry_month = forms.ChoiceField(
         label='Month', choices=MONTH_CHOICES, required=False)
     expiry_year = forms.ChoiceField(
@@ -23,9 +35,51 @@ class OrderForm(forms.ModelForm):
         model = Order
         fields = ('user', 'full_name', 'phone_number', 'street_address1', 'street_address2',
                   'town_or_city', 'postcode', 'county', 'country')
-        # next line found on StackOverflow:
+        # HiddenInput found on StackOverflow:
         # https://stackoverflow.com/questions/15795869/django-modelform-to-have-a-hidden-input
-        widgets = {'user': forms.HiddenInput}
+        widgets = {'user': forms.HiddenInput(),
+                   'full_name': forms.TextInput(attrs={
+                       "class": "form-control",
+                       "aria-label": "Your name",
+                       "placeholder": "Your name"
+                   }),
+                   "phone_number": forms.TextInput(attrs={
+                       "class": "form-control",
+                       "aria-label": "Your phone number",
+                       "placeholder": "Your phone number"
+                   }),
+                   "street_address1": forms.TextInput(attrs={
+                       "class": "form-control",
+                       "aria-label": "Address line 1",
+                       "placeholder": "Address line 1"
+                   }),
+                   "street_address2": forms.TextInput(attrs={
+                       "class": "form-control",
+                       "aria-label": "Address line 2",
+                       "placeholder": "Address line 2"
+                   }),
+                   "town_or_city": forms.TextInput(attrs={
+                       "class": "form-control",
+                       "aria-label": "Town or city",
+                       "placeholder": "Town or city"
+                   }),
+                   "postcode": forms.TextInput(attrs={
+                       "class": "form-control",
+                       "aria-label": "Postcode",
+                       "placeholder": "Postcode"
+                   }),
+                   "county": forms.TextInput(attrs={
+                       "class": "form-control",
+                       "aria-label": "County",
+                       "placeholder": "County"
+                   }),
+                   "country": forms.TextInput(attrs={
+                       "class": "form-control",
+                       "aria-label": "Country",
+                       "placeholder": "Country",
+                       "default": "United Kingdom"
+                   }),
+                   }
 
 
 class ContactDetailsForm(forms.ModelForm):
@@ -35,4 +89,46 @@ class ContactDetailsForm(forms.ModelForm):
                   'town_or_city', 'postcode', 'county', 'country')
         # next line found on StackOverflow:
         # https://stackoverflow.com/questions/15795869/django-modelform-to-have-a-hidden-input
-        widgets = {'user': forms.HiddenInput}
+        widgets = {'user': forms.HiddenInput(),
+                   'full_name': forms.TextInput(attrs={
+                       "class": "form-control",
+                       "aria-label": "Your name",
+                       "placeholder": "Your name"
+                   }),
+                   "phone_number": forms.TextInput(attrs={
+                       "class": "form-control",
+                       "aria-label": "Your phone number",
+                       "placeholder": "Your phone number"
+                   }),
+                   "street_address1": forms.TextInput(attrs={
+                       "class": "form-control",
+                       "aria-label": "Address line 1",
+                       "placeholder": "Address line 1"
+                   }),
+                   "street_address2": forms.TextInput(attrs={
+                       "class": "form-control",
+                       "aria-label": "Address line 2",
+                       "placeholder": "Address line 2"
+                   }),
+                   "town_or_city": forms.TextInput(attrs={
+                       "class": "form-control",
+                       "aria-label": "Town or city",
+                       "placeholder": "Town or city"
+                   }),
+                   "postcode": forms.TextInput(attrs={
+                       "class": "form-control",
+                       "aria-label": "Postcode",
+                       "placeholder": "Postcode"
+                   }),
+                   "county": forms.TextInput(attrs={
+                       "class": "form-control",
+                       "aria-label": "County",
+                       "placeholder": "County"
+                   }),
+                   "country": forms.TextInput(attrs={
+                       "class": "form-control",
+                       "aria-label": "Country",
+                       "placeholder": "Country",
+                       "default": "United Kingdom"
+                   }),
+                   }
