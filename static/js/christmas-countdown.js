ed58b4f5d25ac821574df235492b413e82077b45
Joke Heyndels <jo_hannah@outlook.com>
2020-01-27 21:31:22 +0100

    Add tooltip to ? icon for formfield helptext

--- static/js/christmas-countdown.js
+++ static/js/christmas-countdown.js
@@ -6,3 +6,11 @@ Modified: 03/02/2018
 Original Script: https://www.days-until-christmas.co.uk/webcounts/xcd3248.js */
 
 document.write(); var t = new Date(), td = t.getDate(), tm = t.getMonth(), xy = t.getFullYear(); 11 == tm && td > 25 && xy++; var xd = new Date(xy, 11, 25), msd = 86400000, tl = (xd.getTime() - t.getTime()), dl = tl / msd, wdl = Math.ceil(dl), a = document.getElementById('xcda3248'); 0 == wdl ? document.write("<div style='font-size:16px;left:2px;top:62px;'>Merry<br>Christmas!</div>") : 1 == wdl ? document.write("<div style='font-size:12px;left:2px;top:66px;'>It's<br>Christmas Eve!</div>") : document.write("<div style='font-size:28px;top:65px;'>" + wdl + "</div>"); (!a || a.href.indexOf("www.days-until-christmas.co.uk") < 0) && (document.getElementById("xcd3248").innerHTML = "");
+
+
+/**
+ * Activate all tooltips 
+ */
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
