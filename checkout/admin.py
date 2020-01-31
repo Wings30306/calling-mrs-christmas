e57b1b5cfdc9d8343af0c7e2c8d91622a09ed73c
Joke Heyndels <jo_hannah@outlook.com>
2020-01-31 12:06:11 +0100

    Update admin.py in accounts, checkout for model change

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order, OrderLineItem, ContactDetails
+from .models import Order, OrderLineItem
 
 
 class OrderLineAdminInline(admin.TabularInline):
@@ -11,4 +11,3 @@ class OrderAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Order, OrderAdmin)
-admin.site.register(ContactDetails)
